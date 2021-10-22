14335a5c005a87f46cd89a129e40c5b96bd2094c
devmaster54 <soft.expert32@gmail.com>
2021-10-22 12:49:42 +0800

    update presale contract

--- compiled-contracts/Presale_Swap.sol
+++ compiled-contracts/Presale_Swap.sol
@@ -1,5 +1,5 @@
-pragma solidity ^0.7.4;\n\n// SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.4;
 
 /*
  * @dev Provides information about the current execution context, including the
@@ -719,45 +719,6 @@ contract ERC20 is Context, IERC20 {
         address to,
         uint256 amount
     ) internal virtual {}
-}
-
-/**
- * @dev Extension of {ERC20} that allows token holders to destroy both their own
- * tokens and those that they have an allowance for, in a way that can be
- * recognized off-chain (via event analysis).
- */
-abstract contract ERC20Burnable is Context, ERC20 {
-    using SafeMath for uint256;
-
-    /**
-     * @dev Destroys `amount` tokens from the caller.
-     *
-     * See {ERC20-_burn}.
-     */
-    function burn(uint256 amount) public virtual {
-        _burn(_msgSender(), amount);
-    }
-
-    /**
-     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
-     * allowance.
-     *
-     * See {ERC20-_burn} and {ERC20-allowance}.
-     *
-     * Requirements:
-     *
-     * - the caller must have allowance for ``accounts``'s tokens of at least
-     * `amount`.
-     */
-    function burnFrom(address account, uint256 amount) public virtual {
-        uint256 decreasedAllowance = allowance(account, _msgSender()).sub(
-            amount,
-            "ERC20: burn amount exceeds allowance"
-        );
-
-        _approve(account, _msgSender(), decreasedAllowance);
-        _burn(account, amount);
-    }
 }
 
 /**
@@ -1673,55 +1634,125 @@ abstract contract AccessControl is Context {
     }
 }
 
-contract ARGH_Token is ERC20, AccessControl, ERC20Burnable, Pausable {
+interface AggregatorV3Interface {
+    function decimals() external view returns (uint8);
+
+    function description() external view returns (string memory);
+
+    function version() external view returns (uint256);
+
+    // getRoundData and latestRoundData should both raise "No data present"
+    // if they do not have data to report, instead of returning unset values
+    // which could be misinterpreted as actual reported values.
+    function getRoundData(uint80 _roundId)
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+
+    function latestRoundData()
+        external
+        view
+        returns (
+            uint80 roundId,
+            int256 answer,
+            uint256 startedAt,
+            uint256 updatedAt,
+            uint80 answeredInRound
+        );
+}
+
+contract Presale_Swap is AccessControl, Pausable {
     using SafeMath for uint256;
+    address _tokenArgh;
+    AggregatorV3Interface internal _priceFeed;
+    uint256 _tokenPrice = 0;
+    mapping(address => uint256) _bnbPerUser;
+    uint256 _maxBNB_User = 3e18;
 
-    uint256 internal _maxAmountMintable = 100_000_000e18;
+    /**
+     * Token Price in USD
+     * Decimals: 8
+     */
 
-    constructor() ERC20("PirateWars", "ARGH") {
+    /**
+     * Network: BSC Mainnet
+     * Aggregator: BNB/USD
+     * Address: 0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE
+     */
+
+    /**
+     * Network: BSC Testnet
+     * Aggregator: BNB/USD
+     * Address: 0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526
+     */
+    constructor(address token) {
+        _tokenArgh = token;
+        _priceFeed = AggregatorV3Interface(
+            0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526
+        );
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
     modifier onlyAdminRole() {
-        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "!admin");
+        require(
+            hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
+            "Presale Swap: !admin"
+        );
         _;
     }
 
     function transferOwnership(address newOwner) public onlyAdminRole {
-        require(msg.sender != newOwner, "!same address");
+        require(msg.sender != newOwner, "Presale Swap: !same address");
         grantRole(DEFAULT_ADMIN_ROLE, newOwner);
         revokeRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function mint(address _to, uint256 _amount)
-        public
-        onlyAdminRole
-        whenNotPaused
-    {
+    function swap() public payable whenNotPaused returns (bool) {
+        uint256 bnb_amount = msg.value;
+        (, int256 price, , , ) = _priceFeed.latestRoundData();
+
+        uint256 token_amount = (bnb_amount * uint256(price)) / _tokenPrice;
+
         require(
-            ERC20.totalSupply().add(_amount) <= _maxAmountMintable,
-            "Max mintable exceeded"
+            _bnbPerUser[msg.sender] + bnb_amount <= _maxBNB_User,
+            "Presale Swap: Exceed Max BNB !"
         );
-        super._mint(_to, _amount);
+
+        require(
+            ERC20(_tokenArgh).balanceOf(address(this)) >= token_amount,
+            "Presale Swap: Not enough Argh Token !"
+        );
+        _bnbPerUser[msg.sender] = _bnbPerUser[msg.sender] + bnb_amount;
+        bool res = ERC20(_tokenArgh).transfer(msg.sender, token_amount);
+        return res;
     }
 
-    function transfer(address recipient, uint256 amount)
-        public
-        virtual
-        override
-        returns (bool)
-    {
-        super.transfer(recipient, amount);
-        return true;
+    function setTokenPrice(uint256 price) public onlyAdminRole {
+        _tokenPrice = price;
     }
 
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public virtual override returns (bool) {
-        super.transferFrom(sender, recipient, amount);
-        return true;
+    function getTokenPrice() public view returns (uint256) {
+        return _tokenPrice;
+    }
+
+    function getTokenAmount() public view returns (uint256) {
+        uint256 balance = ERC20(_tokenArgh).balanceOf(address(this));
+        return balance;
+    }
+
+    function getBNBAmount() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function getBNBPrice() public view returns (uint256) {
+        (, int256 price, , , ) = _priceFeed.latestRoundData();
+        return uint256(price);
     }
 
     function pause() external onlyAdminRole {
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,20 @@
 // const Cifi_Token = artifacts.require("Cifi_Token");
 const ARGH_Token = artifacts.require("ARGH_Token");
+const Presale_Swap = artifacts.require("Presale_Swap");
 
 module.exports = async function (deployer) {
   const accounts = await web3.eth.getAccounts();
   console.log(accounts[0]);
   await deployer.deploy(ARGH_Token, { from: accounts[0], overwrite: false });
+
+  console.log("token address: ", ARGH_Token.address);
+  await deployer.deploy(Presale_Swap, ARGH_Token.address, {
+    from: accounts[0],
+    overwrite: false,
+  });
+  const swap = await Presale_Swap.deployed();
+
+  console.log("BNB price: ", String(await swap.getBNBPrice()));
   // const argh_token = await ARGH_Token.deployed();
   // const multisignWallet = "0x49c38843b433FcbA6C5fa5876E98350Bf68E6bE6";
   // await argh_token.mint(multisignWallet, BigInt(10 ** 26), {
